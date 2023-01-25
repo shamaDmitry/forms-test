9559a6621a33cde57f353bc7c8b4b756b4a66270
shama <dmitry.shama@faceit.com.ua>
2023-01-25 17:43:35 +0200

    fix localstorage

--- src/main.jsx
+++ src/main.jsx
@@ -4,22 +4,24 @@ import App from './App'
 import { getAllCountries } from './API'
 import './style.css'
 
-
-let data = (function () {
+let data = async function () {
   let countries = localStorage.getItem('countries');
 
   if (!countries) {
-    getAllCountries().then(res => {
-      localStorage.setItem('countries', JSON.stringify(res))
-    })
+    const res = await getAllCountries();
+    localStorage.setItem('countries', JSON.stringify(res))
+    let countries = localStorage.getItem('countries');
+    
+    return JSON.parse(countries)
   }
-  return JSON.parse(countries);
 
-  // return []
-})();
+  return JSON.parse(countries)
+};
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <App initialData={data} />
-  // </React.StrictMode>,
-)
+data().then(res => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App initialData={res} />
+    </React.StrictMode>,
+  )
+})
